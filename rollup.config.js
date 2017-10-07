edbf9ad996bad4984b158873c11d2274a3bac13b
JerryHong <s6323859@gmail.com>
2017-10-08 03:32:54 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'cjs',
+    format: 'iife',
   },
   sourcemap: true,
   plugins: [
-    resolve(),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
     sourcemaps(),
     babel({
       exclude: 'node_modules/**', // only transpile our source code
